65761f8121d86d598c82b5eda74d4f40721b9fcc
Marcus Little <mcolelittle@gmail.com>
2026-02-06 13:53:58 -0500

    adding headers to toml

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=30; includeSubDomains; preload',
-          },
-        ],
-      },
-    ]
-  },
 };
 
 export default nextConfig;
